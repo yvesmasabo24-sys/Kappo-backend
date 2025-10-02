1225df5036b9d55a322ecfaf3f8f9c6650ebe3ac
yvesmasabo24-sys <yvesmasabo24@gmail.com>
2025-10-02 23:13:46 +0200

    backend

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,17 @@ import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/databaseConfiguration";
-import authRoute from "./routes/AuthRoute";
+//import authRoute from "./routes/AuthRoute";
+import router from "./routes/AuthRoute";
+
+
 import cartRoutes from "./routes/cartRoute"; 
-import productRoutes from "./routes/productRoute"; // ✅ import product routes
+import Router from "./routes/ProductRoute"; // fix case for Linux/Render
 import testRoutes from "./routes/testRoutes";
 import orderRoutes from "./routes/orderRoute";
-import seedroutes from "./routes/seedRoutes";
 import mainRouter from "./routes/indexRouting";
 import contactRouter from "./routes/contactRoute";
-import {swaggerUi, swaggerDocs } from "../src/documentetion/swagger"
+import { swaggerUi, swaggerDocs } from "./documentetion/swagger";
 dotenv.config();
 
 const app: Application = express();
@@ -18,33 +20,29 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
-
-app.use(
-  cors()
-);
+app.use(cors());
 
 app.use(express.json());
 
 // ✅ Existing routes
-app.use("/api-v1/users", authRoute);
+app.use("/api-v1/users", router);
 
 // ✅ Product routes
-app.use("/api-v1/products", productRoutes);
+app.use("/api-v1/products", router);
 
 // ✅ Cart routes
 app.use("/api-v1/cart", cartRoutes);
 
-// ✅ Add order routes
+// ✅ Order routes
 app.use("/api-v1/orders", orderRoutes);
 
-import seedRoutes from "./routes/seedRoutes";
 //testing routes
 app.use("/api-v1/test", testRoutes);
 app.use("/api-v1", mainRouter);
 app.use("/api-v1/contact", contactRouter);
 
-app.use("/docs",swaggerUi.serve,swaggerUi.setup(swaggerDocs));
+// Swagger docs
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is running!");
